e82a44e57c002d1aebae78123d42bb7e925b8797
diegonti <diegontidoc2000@gmail.com>
2023-02-22 15:26:12 +0100

    finished. bugs fixed. Implemented each optimization next case. To implement: functions, comments, sh script for general (sys.arg?) append geoms to common file

--- optimizer.py
+++ optimizer.py
@@ -19,25 +19,24 @@ import shutil
 
 from VASPread import OUTCAR
 from structure import CONTCAR
-from VASP import MX
 
-path = "~/test/Cr3C2/ABC/"
-path1 = path + "opt/"     # change this to the folder 
+home = os.path.expanduser("~")
+path = f"{home}/test/Cr3C2/ABC/"
+path1 = path + "opt/"
 original_cwd = os.getcwd()
 
 extension = ""
 
 while True:
-    # cwd ?
+
+    path_outcar = path1 + f"{extension}OUTCAR"
 
     os.chdir(path1 + extension)
     poscar = CONTCAR("POSCAR")
-    os.system(f"qsub -N opt{poscar.name} script")
+    if os.path.exists(path_outcar): pass
+    else: os.system(f"qsub -N {poscar.name} script")
 
-    path_outcar = path1 + f"{extension}OUTCAR"
-    while not os.path.exists(path_outcar):
-        time.sleep(1)
-    print("File found")
+    while not os.path.exists(path_outcar): time.sleep(1)
 
     outcar = OUTCAR("OUTCAR")
     pressures, forces, last_pressure, next_opt = outcar.getOpt()
@@ -45,27 +44,37 @@ while True:
 
     if next_opt == "optimized": 
 
-        shutil.copy("OUTCAR",path1)
-        shutil.copy("CONTCAR",path1)
-        E, = outcar.getEnergy()
+        shutil.copy("OUTCAR",path)
+        shutil.copy("CONTCAR",path)
+        E,energies = outcar.getEnergy()
 
         contcar = CONTCAR("CONTCAR")
         geom = f"{contcar.mx.mxName}: {contcar.getGeom()}\n"
         with open(path+"geom","a") as outFile: outFile.write(geom)
 
-        print("Process optimized")
+        print(f"\u2713 {path}: Process optimized")
         break
 
-    os.mkdir(next_opt)
-    os.system(f"cpvasp {next_opt}")
+    try: os.mkdir(next_opt)
+    except FileExistsError: pass
+    # os.system(f"{home}/scripts/cpvasp.sh {next_opt}") #change by shutil
+    shutil.copy("KPOINTS",next_opt+"/")
+    shutil.copy("POTCAR",next_opt+"/")
+    shutil.copy("script",next_opt+"/")
+    shutil.copy("CONTCAR",next_opt+"/POSCAR")
+
     if next_opt == "isif7": 
         extension += "isif7/"
-        os.system(f"sed '/ISIF/c\ISIF = 7' INCAR | sed '/NSW/c\NSW = 19' > {next_opt}/INCAR")
+        os.system(rf"sed '/ISIF/c\ISIF = 7' INCAR | sed '/NSW/c\NSW = 19' > {next_opt}/INCAR")
 
     elif next_opt == "isif2": 
         extension += "isif2/"
-        os.system(f"sed '/ISIF/c\ISIF = 2' INCAR | sed '/NSW/c\NSW = 101' > {next_opt}/INCAR")
-    # change INCAR
+        os.system(rf"sed '/ISIF/c\ISIF = 2' INCAR | sed '/NSW/c\NSW = 101' > {next_opt}/INCAR")
+
+    elif next_opt == "isif7a":
+        extension += next_opt + "/"
+        os.system(rf"sed '/NSW/c\NSW = 15' INCAR > {next_opt}/INCAR")
+
     
 
 
