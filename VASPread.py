bb8ad6367ecd7a17b1bc20df5defe3a9843eba23
diegonti <diegontidoc2000@gmail.com>
2023-02-22 15:23:10 +0100

    Added documentation

--- VASPread.py
+++ VASPread.py
@@ -6,14 +6,17 @@ Diego Ontiveros
 """
 class OUTCAR():
     def __init__(self,path) -> None:
+        """
+        Creates OUTCAR object. Allows to quickly get the important information of the OUTCAR.
+        `path` : path to the OUTCAR file."""
 
         self.path = path
         self.data,self.raw_data = self.getData()
 
 
-        pass
-
     def getData(self):
+        """Reads the OUTCAR and returns an out list of the 
+        raw lines (raw_data) and the .strip().split() lines (data). """
 
         data = []
         with open(self.path,"r") as inFile:
@@ -54,13 +57,17 @@ class OUTCAR():
         else: search_until = True
         found = False
         
+        # Main search loop
         out, out_raw = [],[]
         i_line,i_until = [],[]
         for i,line in enumerate(self.raw_data):
+
+            # Save line number when the target is found
             if target in line and not found:
                 found = True
                 i_line.append(i)
 
+            # Continuation for each search case
             if found and not search_until and not repeat: 
                 i_until.append(i_line[-1])
                 break
@@ -75,11 +82,7 @@ class OUTCAR():
                     if not repeat: break
                     elif repeat: continue
 
-        # test        
-        # print(i_line)
-        # print(i_until)
-        # print(len(i_line)==len(i_until))
-
+        # Selecting the pieces of data to gather
         for i,j in zip(i_line,i_until):
             dat = self.data[i-before:j+after+1] 
             raw_dat = self.raw_data[i-before:j+after+1]
@@ -90,55 +93,74 @@ class OUTCAR():
 
         return out, out_raw
     
-    def getOpt(self):
 
+    def getOpt(self,print_info=False):
+        """Gets the optimization data (external pressure and forces) from the OUTCAR file.
+        Also, returns the next step of the optimization (if not finished).
+        
+        Returns
+        ---------
+        `pressures` : List with the external pressures of each optimization step.
+        `forces` : List of the forces of the last optimization step.
+        `last_pressure` : External pressure of the last step.
+        `next_optimize` : Optimization type to do next (isif2 or isif7). "optimized" if all optimized.  
+        """
+        # Getting external pressure and forces data
         pressure, pressure_raw = self.search("external pressure")
         force, force_raw = self.search("TOTAL-FORCE",until="total drift")
 
+        # Creating list of pressures
         pressures = []
         for p in pressure:
             pi = float(p[0][3])
             pressures.append(pi)
 
+        # Creating list of forces
         forces = []
         for f in force[-1][2:-2]:
             fi = float(f[-1])
             forces.append(fi)
 
+        # Getting absolute values
         pressures_abs = [abs(p) for p in pressures]
         forces_abs = [abs(f) for f in forces]
         forces_max = [f for f in forces_abs if f>0.01]
         last_pressure = pressures[-1]
 
-        print("Pressures (kB):\n", pressures)
-        print("Forces (eV/Ang):\n", forces)
-
+        # Optimization results depending on each case
         next_optimize = ""
         if abs(last_pressure) <= 1.00: print("\u2713 Pressure optimized.")
+        elif abs(last_pressure) == abs(pressures[0]) and abs(pressures[-2]) <= 1.00 and abs(pressures[-1]) >= 1.00:
+            next_optimize = "isif7a"
+            if print_info: print("\u2717 Redoing isif7.")
         else: 
-            print("\u2717 Pressure NOT optimized. Try isif7.")
             next_optimize = "isif7"
+            if print_info: print("\u2717 Pressure NOT optimized. Try isif7.")
 
         if len(forces_max) > 0: 
-            print("\u2717 Forces NOT optimized. Try isif2.")
             next_optimize = "isif2"
+            if print_info: print("\u2717 Forces NOT optimized. Try isif2.")
         else: 
-            print("\u2713 Forces optimized.")
+            if print_info: print("\u2713 Forces optimized.")
 
         if next_optimize == "": next_optimize = "optimized"
-            
 
+        if print_info:
+            print("Pressures (kB):\n", pressures)
+            print("Forces (eV/Ang):\n", forces)
 
         return pressures, forces, last_pressure, next_optimize
 
     def getEnergy(self):
-        
+        """Gets the energies (E0) for each optimization step.
+        `final_energy` is the optimized one, while all the energies are found in `energies`.
+        """
         energies, energies_raw = self.search("energy(sigma->0)")
 
         energies = [float(e[0][-1]) for e in energies]
         final_energy = energies[-1]
 
-        return final_energy,energies
+        return final_energy, energies
         
 
 ############################## MAIN PROGRAM #######################
