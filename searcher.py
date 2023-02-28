5bfae0e312f3a2ea4a90b5f51bb7f648cc0786e7
diegonti <diegontidoc2000@gmail.com>
2023-02-28 15:56:16 +0100

    added moveHome case

--- searcher.py
+++ searcher.py
@@ -6,8 +6,10 @@ Diego Ontiveros
 """
 
 import os
+import shutil
 
 home = os.path.expanduser("~")
+os.chdir(home)
 
 dirsMXT = ["DOS/","DOS/PBE0/","BS/PBE/","BS/PBE0/","BS/PBE/BS2/","BS/PBE0/BS2/","WF/"]
 dirsMXT = ["DOS/","DOS/PBE0/","BS/"]
@@ -23,13 +25,22 @@ def parseTermination(T):
 def parseTarget(target:str):
     """Parses the target input to search and decides extensions to search."""
     
-    if target.lower().startswith("dos") and not target.endswith("0"): extras = ["DOS/DOSCAR"]
-    elif target.lower().startswith("dos") and target.endswith("0"): extras = ["DOS/PBE0/DOSCAR"]
-    elif target.lower().startswith("cont"): extras = ["CONTCAR"]
-    elif target.lower().startswith("loc"): extras = ["WF/LOCPOT"]
-    elif target.lower().startswith("bader"): extras = ["bader/ACF.dat"]
+    if target.lower().startswith("dos") and not target.endswith("0"): extras = "DOS/DOSCAR"
+    elif target.lower().startswith("dos") and target.endswith("0"): extras = "DOS/PBE0/DOSCAR"
+    elif target.lower().startswith("cont"): extras = "CONTCAR"
+    elif target.lower().startswith("loc"): extras = "WF/LOCPOT"
+    elif target.lower().startswith("bader"): extras = "bader/ACF.dat"
 
     return extras
+
+# def selectOut(path,pristine):
+#     if pristine:
+#         if "ABA" in
+#         pass
+#     else:
+#         pass
+
+#     return out_folder
 
 class SEARCH():
 
@@ -42,6 +53,7 @@ class SEARCH():
         self.n = None
         self.T = None
         self.pristine = None
+        self.data = None
         self.search_paths = None
         self.target = None
 
@@ -88,7 +100,7 @@ class SEARCH():
                         
                         path = f"{home}/M{n+1}X{n}/{mx}/{stack}/"
                         paths.append(path)
-                        data.append([mx,stack]) # data.append([mx,stack,""])
+                        data.append([mx,stack,""]) # data.append([mx,stack,""])
 
         # For Terminated cases
         else:
@@ -106,35 +118,39 @@ class SEARCH():
 
     def search(self,target:str,n:int,T:str=None):
 
-        self.target = target
         T, pristine = parseTermination(T)
+        self.n, self.T = n,T
 
         paths, data = self.path_tree(n,T)
 
         extras = parseTarget(target)
+        self.target = extras.split("/")[-1]
 
         counter = 0
-        search_paths = []
-        for path in paths:
+        search_paths, search_data = [], []
+        for i,path in enumerate(paths):
             if os.path.exists(path): 
                 counter += 1
                 search_paths.append(path + extras)
+                search_data.append(data[i])
             else: print(f"{path} : Path not found.")
-        print(f"Found {counter} files from the total {len(paths)}")
+        print(f"Searched n = {n} T = {T}: Found {counter} files from the total {len(paths)}.")
 
         self.search_paths = search_paths
-        return paths
+        self.search_data = search_data
+        return paths, search_data 
 
-    def move(self,destination:str,n:int=None,T:str=None,name:str=None):
+    def move(self,destination:str,n:int,T:str=None,name:str=None):
 
         search_paths = self.search_paths
+        T, pristine = parseTermination(T)
         if name is None: name = self.target
 
         
-        if destination.lower().startswith("home"):
-        
-            pass
-        if destination.lower().startswith("mx"):pass
+        if destination.lower().startswith("home"): self.moveHome()
+
+        if destination.lower().startswith("mx"):
+            destination_paths,data = self.path_tree(n,T)
 
         # for folder in new_folders:
         #   try: os.mkdir(folder)
@@ -142,6 +158,36 @@ class SEARCH():
 
         # for path in move_paths:pass
             ## asegurarse que sean el mismo MX
+
+    def moveHome(self,path_folders:str=None,name:str=None):
+        n = self.n
+        T, pristine = parseTermination(self.T)
+        if name is None: name = self.target
+        search_paths = self.search_paths
+        search_data = self.search_data
+
+        if path_folders is None: path_folders = f"{home}/searcher/"
+
+        try: os.mkdir(path_folders)
+        except FileExistsError: pass
+        os.chdir(path_folders)
+
+        # Creating folders
+        if pristine: folders = mx_folders
+        else: folders = mxt_folders
+        for folder in folders:
+            try: os.mkdir(folder)
+            except FileExistsError: pass
+
+        for path,data in zip(search_paths, search_data):
+
+            # Selct out folder
+            if pristine: out_folder = data[1]+" "+data[2]+"/"
+            else: out_folder = data[1]+" "+data[2]+"/"
+
+            # copy from search path to destination
+            shutil.copy(path,f"{out_folder}/{name}_{data[0]}")
+            
 
 
 ############################### MAIN PROGRAM ###############################
